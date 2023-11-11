0a12ef07d6b5005532af4e813eb6f62854854f13
LoftyCode <138039034+LoftyCode@users.noreply.github.com>
2023-11-11 16:21:14 +0800

    Update save_hwid.php

--- save_hwid.php
+++ save_hwid.php
@@ -1,43 +1,23 @@
 <?php
-// Этот файл принимает POST-запросы с ключем, проверяет, не привязан ли данный ключ к хвиду,
-// и если не привязан, привязывает к хвиду и возвращает его
+// Получаем ключ и хвид из POST-запроса
+$key = $_POST['key'];
+$hwid = $_POST['hwid'];
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $key = $_POST['key'];
-
-    // Проверяем, что ключ не пуст
-    if (!empty($key)) {
-        // Открываем файл с ключами и хвидами
-        $keysFile = file_get_contents('keys.txt');
-        $keys = explode("\n", $keysFile);
-
-        // Проверяем, не привязан ли данный ключ к хвиду
-        foreach ($keys as $line) {
-            list($savedKey, $hwid) = explode('=', $line);
-            if ($key === $savedKey) {
-                // Если ключ уже привязан, возвращаем ошибку
-                echo json_encode(['error' => 'Key already bound to HWID']);
-                exit;
-            }
-        }
-
-        // Если ключ не привязан, генерируем новый хвид
-        $newHwid = bin2hex(random_bytes(16));
-
-        // Добавляем новую пару ключ-хвид в файл
-        file_put_contents('keys.txt', "\n{$key}={$newHwid}", FILE_APPEND);
-
-        // Добавляем хвид в файл с привязанными хвидами
-        file_put_contents('hwids.txt', "{$newHwid}\n", FILE_APPEND);
-
-        // Возвращаем новый хвид
-        echo json_encode(['success' => true, 'hwid' => $newHwid]);
-    } else {
-        // Возвращаем ошибку, если ключ пуст
-        echo json_encode(['error' => 'Key cannot be empty']);
-    }
+// Проверяем, что ключ и хвид переданы
+if ($key && $hwid) {
+    // Открываем файл keys.txt для добавления новой строки
+    $file = fopen("keys.txt", "a");
+    
+    // Пишем ключ и хвид в файл
+    fwrite($file, $key . '=' . $hwid . "\n");
+    
+    // Закрываем файл
+    fclose($file);
+    
+    // Возвращаем успешный ответ
+    echo "success";
 } else {
-    // Возвращаем ошибку для неподдерживаемых запросов
-    echo json_encode(['error' => 'Unsupported request method']);
+    // Возвращаем ошибку, если ключ или хвид не переданы
+    echo "error";
 }
 ?>
